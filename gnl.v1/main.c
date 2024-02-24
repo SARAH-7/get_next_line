831c8e9d3baff1b16f86a71f2fed944d2e83b496
SARAH-7 <62673148+SARAH-7@users.noreply.github.com>
2024-02-24 21:23:24 +0400

    Add files via upload

---
+++ gnl.v1/main.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sbakhit <sbakhit@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/05 17:21:13 by sbakhit           #+#    #+#             */
+/*   Updated: 2024/02/24 21:12:12 by sbakhit          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h" 
+
+int	main(void)
+{
+	int		fd;
+	char	*line;
+	int		count;
+
+	count = 0;
+	fd = open("hi.txt", O_RDONLY);
+	if (fd == -1)
+		return (-1);
+	while ((line = get_next_line(fd)) != NULL)
+	{
+		count++;
+		printf("[%d]%s\n", count, line);
+		free(line);
+	}
+	close(fd);
+	return (0);
+}
