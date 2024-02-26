58492c59cc5cd03221e51f09cb3a16e0c6cf67cf
SARAH-7 <62673148+SARAH-7@users.noreply.github.com>
2024-02-26 18:18:12 +0400

    Add files via upload

--- gnl.v1/get_next_line.c
+++ gnl.v1/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: sbakhit <sbakhit@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/28 13:54:44 by sbakhit           #+#    #+#             */
-/*   Updated: 2024/02/25 18:52:35 by sbakhit          ###   ########.fr       */
+/*   Updated: 2024/02/26 17:18:15 by sbakhit          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,8 @@ void	get_the_next_line(int bytes_read, char **line,
 	char	*temp;
 	char	*temp1;
 
-	temp1 = ft_strchr(*line, '\n');
 	temp = NULL;
+	temp1 = ft_strchr(*line, '\n');
 	while (bytes_read > 0 || *remain != NULL)
 	{
 		if (*remain)
@@ -38,6 +38,7 @@ void	get_the_next_line(int bytes_read, char **line,
 			*line = assign_line(temp, *buffer);
 			free(temp);
 			temp = NULL;
+			// temp1 = ft_strchr(*line, '\n');
 		}
 		if (temp1)
 		{
@@ -71,16 +72,8 @@ char	*get_next_line(int fd)
 	if (!buffer)
 		return (NULL);
 	bytes_read = read(fd, buffer, BUFFER_SIZE);
-	if (bytes_read <= 0)
-	{
-		free(temp);
-		free(buffer);
-		free(remain);
-		return (NULL);
-	}
 	while (bytes_read > 0 || remain != NULL)
 	{
-		buffer[bytes_read] = '\0';
 		get_the_next_line(bytes_read, &line, &buffer, &remain);
 		temp = ft_strchr(line, '\n');
 		if (temp)
--- gnl.v1/main.c
+++ gnl.v1/main.c
@@ -6,7 +6,7 @@
 /*   By: sbakhit <sbakhit@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/05 17:21:13 by sbakhit           #+#    #+#             */
-/*   Updated: 2024/02/25 17:50:22 by sbakhit          ###   ########.fr       */
+/*   Updated: 2024/02/26 12:04:33 by sbakhit          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ int	main(void)
 	while ((line = get_next_line(fd)) != NULL)
 	{
 		count++;
-		printf("[%d]%s", count, line);
+		printf("[%d](%s)", count, line);
 		free(line);
 		line = NULL;
 	}
